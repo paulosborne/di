188b4d633ad2c4f589b83fafe728d63a9373c818
Phil Bennett <phil@happyaccidents.me>
2013-02-22 19:28:01 +0000

    Added comments to ContainerInterface

--- src/Orno/Di/ContainerInterface.php
+++ src/Orno/Di/ContainerInterface.php
@@ -2,7 +2,21 @@
 
 interface ContainerInterface
 {
+    /**
+     * Register an item with the container
+     *
+     * @param  string|closure|object $alias
+     * @param  string|closure|object $object
+     * @param  boolean               $shared
+     * @return Orno\Di\Definition
+     */
     public function register($alias, $object = null, $shared = false);
 
+    /**
+     * Resolve an item from the container
+     *
+     * @param  string $alias
+     * @return object
+     */
     public function resolve($alias);
 }
