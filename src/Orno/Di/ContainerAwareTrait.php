af6e4129c002158b17b535bac0c44a73644e2f3f
Phil Bennett <philipobenito@gmail.com>
2013-03-05 13:38:03 +0000

    Added setContainer method to ContainerAwareTrait

--- src/Orno/Di/ContainerAwareTrait.php
+++ src/Orno/Di/ContainerAwareTrait.php
@@ -21,6 +21,12 @@ trait ContainerAwareTrait
         return $this->container;
     }
     
+    /**
+     * Set the container
+     * 
+     * @param  Container\Di\ContainerInterface
+     * @return void
+     */
     public function setContainer(ContainerInterface $container)
     {
         $this->container = $container;
